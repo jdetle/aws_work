35ffb29917e3733a5040ca9f8e28790dd2eec991
John Detlefs <jdetlefs@godaddy.com>
2022-10-15 11:21:26 -0500

    Save copypasta

--- lib/aws_work-stack.ts
+++ lib/aws_work-stack.ts
@@ -1,16 +1,72 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
 export class AwsWorkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const APP_PORT = 3000
+    const pathToDockerFile = "../gasket-frontend-yarn"
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 2,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsWorkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const taskDefinition = new ecs.FargateTaskDefinition(this, "MyTaskDefinition", {
+      memoryLimitMiB: 512,
+      cpu: 256,
+    });
+
+    const dockerFile = new DockerImageAsset(this, 'DockerFileAsset', {
+      directory: pathToDockerFile,
+      file: 'Dockerfile',
+    });
+
+    // cdk will build it and push it to en ecr repository
+    const image = ecs.ContainerImage.fromDockerImageAsset(dockerFile);
+
+    const container = taskDefinition.addContainer("MyContainer", {
+      image,
+      // store the logs in cloudwatch 
+      logging: ecs.LogDriver.awsLogs({ streamPrefix: "myexample-logs" })
+    });
+
+    container.addPortMappings({
+      containerPort: APP_PORT,
+    });
+
+    const cluster = new ecs.Cluster(this, "MyECSCluster", {
+      clusterName: "MyECSCluster",
+      containerInsights: true,
+      vpc,
+    });
+
+    const securityGroup = new ec2.SecurityGroup(this, `My-security-group`, {
+      vpc: vpc,
+      allowAllOutbound: true,
+      description: 'My Security Group'
+    });
+
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(APP_PORT));
+
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+      cluster,
+      publicLoadBalancer: true,
+      cpu: 256,
+      desiredCount: 1,
+      memoryLimitMiB: 512,
+      taskDefinition,
+      securityGroups: [securityGroup]
+    })
+
+    const scalableTarget = fargateService.service.autoScaleTaskCount({
+      minCapacity: 1,
+      maxCapacity: 2
+    })
+
+    scalableTarget.scaleOnCpuUtilization('cpuScaling', {
+      targetUtilizationPercent: 70
+    })
   }
 }
